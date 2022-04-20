73cc30ba2b4024944b65aafabd0fdbc3aedc85c4
ShreeyansB <sbahadkar@rocketmail.com>
2022-04-20 16:45:03 +0530

    :sparkles: Added SideArt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Box, Flex, Heading, HStack, VStack } from "@chakra-ui/react";
 import React from "react";
 import Header from "./components/header/Header";
+import SideArt from "./components/side_art/SideArt";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Box h="100vh" w="100%">
-        <HStack h="100%">
-          <Box bg="green.300" h="100%" w="540px"></Box>
-        </HStack>
-      </Box>
+      <SideArt />
     </React.Fragment>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import { Button, HStack, Icon, useColorMode } from "@chakra-ui/react";
+import { Button, HStack, Icon, Link, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import { CgDarkMode } from "react-icons/cg";
 import BrandLogo from "./../../svg/BrandLogo";
@@ -12,14 +12,16 @@ const Header = () => {
   return (
     <HStack
       w="100%"
-      py={4}
+      py={5}
       justify="space-between"
-      px={{ base: '1.5rem', md: "3rem" }}
+      px={{ base: "1.5rem", md: "3rem" }}
       zIndex={2}
       position="absolute"
       top={0}
     >
-      <BrandLogo width="80px" />
+      <Link href="/">
+        <BrandLogo width="80px" />
+      </Link>
       <Button variant="ghost" onClick={themeToggleHandler}>
         <Icon as={CgDarkMode} w="1.3rem" h="1.3rem" />
       </Button>
